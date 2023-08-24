8130f83fc32b17781791932c1106c6614a4e2e14
Giovanni Silva <no-reply@gsilva.pro>
2023-08-24 02:10:57 -0300

    only build versions that are not in registry

--- check_docker_tag.sh
+++ check_docker_tag.sh
@@ -2,8 +2,10 @@
 set -eu -o pipefail
 
 function docker_tag_exists() {
-    curl --silent -f -lSL -H "Authorization: Bearer $GITHUB_TOKEN" https://ghcr.io/v2/$1/manifests/$2 > /dev/null
+  curl --silent -f -lSL -H "Authorization: Bearer $GITHUB_TOKEN" https://ghcr.io/v2/$1/manifests/$2 > /dev/null
 }
+
+echo "Checking if image https://ghcr.io/v2/$1/manifests/$2 exists"
 
 if docker_tag_exists $1 $2; then
   echo "Image exits"
